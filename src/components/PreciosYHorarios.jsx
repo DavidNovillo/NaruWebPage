e9e37cfd5fa877ccf6ec17e4539a8a49ede96fe4
David-Tekku <davidnovillo90@hotmail.com>
2021-03-12 19:08:58 -0500

    aumento de widget de triib

--- src/components/PreciosYHorarios.jsx
+++ src/components/PreciosYHorarios.jsx
@@ -7,6 +7,19 @@ function PreciosYHorarios() {
     <div>
       <Precios />
       <Horarios />
+      <h2 style={{ textAlign: "center", padding: "30px 0 20px" }}>CRONOGRAMA</h2>
+      <iframe
+        style={{ backgroundColor: "black" }}
+        id="blockrandom"
+        name="iframe"
+        src="https://naru-crossfit.triib.com/schedule-embed/"
+        width="100%"
+        height="900"
+        scrolling="yes"
+        frameborder="1"
+        class="wrapper"
+        title="schedule"
+      ></iframe>
     </div>
   );
 }
